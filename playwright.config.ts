61e484cff65c98315d52cd48847dc157311de6b0
godOfRams <bohdan.baran.work@gmail.com>
2024-01-22 17:58:41 +0200

    deleted run server command from playwright conf file

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,10 +28,4 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
   ],
-    // Run your local dev server before starting the tests.
-    webServer: {
-      command: 'yarn dev',
-      url: 'http://127.0.0.1:3000',
-      reuseExistingServer: !process.env.CI,
-    },
 });
