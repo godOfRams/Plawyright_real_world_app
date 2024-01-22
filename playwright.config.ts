75ccadec9ac4139ddb17bd408de9c58df3200156
godOfRams <bohdan.baran.work@gmail.com>
2024-01-22 17:43:01 +0200

    Added the server start before tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,4 +28,10 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
   ],
+    // Run your local dev server before starting the tests.
+    webServer: {
+      command: 'yarn dev',
+      url: 'http://127.0.0.1:3000',
+      reuseExistingServer: !process.env.CI,
+    },
 });
